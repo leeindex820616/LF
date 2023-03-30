50695348a69bc3098b67b136368fdea3cd58bb58
leeindex820616 <127458122+leeindex820616@users.noreply.github.com>
2023-03-30 13:30:51 +0800

    Add files via upload

--- Get_NAS_SSL/grab_file2.py
+++ Get_NAS_SSL/grab_file2.py
@@ -18,12 +18,14 @@ destination_folder = folder_name
 with open(filename_txt, "r") as f1, open(filename_txt2, "w") as f2:
     for line in f1:
         # Remove spaces from line
-        line = line.replace(" ", "")
+        line = line.replace(" ", "").lower()
         # Write updated line to output file
-        f2.write(line)
+        f2.write("." + line)
 # read the list of file names from the text file
 with open(filename_txt2, "r") as f:
     filenames = f.read().splitlines()
+    
+
 
 # iterate over the list of file names
 for filename in filenames:
@@ -32,19 +34,22 @@ for filename in filenames:
     for root, dirs, files in os.walk(nas_path):
         if "OLD" in dirs:
             dirs.remove("OLD")
+        if "RB88" in dirs:
+            dirs.remove("RB88")
         for file in files:
             if filename in file:
                 matching_files.append(os.path.join(root, file))
     
     # if matching files were found, copy the latest one to the destination folder
     if matching_files:
-        latest_file = max(matching_files, key=os.path.getctime)
+        latest_file = max(matching_files, key=os.path.getmtime)
         src_path = os.path.join(nas_path, latest_file)
         dst_path = os.path.join(destination_folder, latest_file)
         shutil.copy2(latest_file, destination_folder)
         print("Copied file:", latest_file)
     else:
-        print("No file found for:", line)    
+        print("No file found for:", filename)
+        
 
 os.remove(filename_txt2)
 # Keep the console open
