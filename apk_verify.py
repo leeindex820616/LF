dc511ce980986552cd472433ae95a26a48143544
leeindex820616 <127458122+leeindex820616@users.noreply.github.com>
2024-04-03 18:52:03 +0800

    Update apk_verify.py

--- apk_verify.py
+++ apk_verify.py
@@ -45,6 +45,7 @@ def check_bundle_name(bundle_content):
 #    root.destroy()
 
 if __name__ == "__main__":
+    print(len(sys.argv))
     if len(sys.argv) != 2:
         print("Usage: python script_name.py path_to_app_file.apk(or ipa)")
         sys.exit(200)
